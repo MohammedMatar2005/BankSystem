87ce25ef6570dafc1c8918c13133eeda4d23133c
mohammed <matarmohammed567@gmail.com>
2026-01-20 15:46:15 +0200

    Create The manage clients form

---
+++ BankSystem/BankSystem/Clients/frmListClients.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace BankSystem.Clients
+{
+    partial class frmListClients
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnAddNewClient = new System.Windows.Forms.Button();
+            this.cbIsActive = new System.Windows.Forms.ComboBox();
+            this.cbFilterBy = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtFilterValue = new System.Windows.Forms.TextBox();
+            this.dgvPeople = new System.Windows.Forms.DataGridView();
+            this.pbUserImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPeople)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUserImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddNewClient
+            // 
+            this.btnAddNewClient.Location = new System.Drawing.Point(826, 227);
+            this.btnAddNewClient.Name = "btnAddNewClient";
+            this.btnAddNewClient.Size = new System.Drawing.Size(81, 74);
+            this.btnAddNewClient.TabIndex = 131;
+            this.btnAddNewClient.UseVisualStyleBackColor = true;
+            // 
+            // cbIsActive
+            // 
+            this.cbIsActive.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbIsActive.FormattingEnabled = true;
+            this.cbIsActive.Items.AddRange(new object[] {
+            "All",
+            "Yes",
+            "No"});
+            this.cbIsActive.Location = new System.Drawing.Point(341, 280);
+            this.cbIsActive.Name = "cbIsActive";
+            this.cbIsActive.Size = new System.Drawing.Size(121, 21);
+            this.cbIsActive.TabIndex = 130;
+            this.cbIsActive.Visible = false;
+            // 
+            // cbFilterBy
+            // 
+            this.cbFilterBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFilterBy.FormattingEnabled = true;
+            this.cbFilterBy.Items.AddRange(new object[] {
+            "None",
+            "User ID",
+            "UserName",
+            "Person ID",
+            "Full Name",
+            "Is Active"});
+            this.cbFilterBy.Location = new System.Drawing.Point(108, 280);
+            this.cbFilterBy.Name = "cbFilterBy";
+            this.cbFilterBy.Size = new System.Drawing.Size(210, 21);
+            this.cbFilterBy.TabIndex = 129;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 281);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 128;
+            this.label3.Text = "Filter By:";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblTitle.Location = new System.Drawing.Point(345, 227);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(258, 39);
+            this.lblTitle.TabIndex = 127;
+            this.lblTitle.Text = "Manage Clients";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtFilterValue
+            // 
+            this.txtFilterValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFilterValue.Location = new System.Drawing.Point(341, 281);
+            this.txtFilterValue.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtFilterValue.Name = "txtFilterValue";
+            this.txtFilterValue.Size = new System.Drawing.Size(238, 20);
+            this.txtFilterValue.TabIndex = 125;
+            // 
+            // dgvPeople
+            // 
+            this.dgvPeople.AllowUserToAddRows = false;
+            this.dgvPeople.AllowUserToDeleteRows = false;
+            this.dgvPeople.AllowUserToResizeRows = false;
+            this.dgvPeople.BackgroundColor = System.Drawing.Color.White;
+            this.dgvPeople.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPeople.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvPeople.Location = new System.Drawing.Point(12, 312);
+            this.dgvPeople.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvPeople.MultiSelect = false;
+            this.dgvPeople.Name = "dgvPeople";
+            this.dgvPeople.ReadOnly = true;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPeople.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvPeople.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPeople.Size = new System.Drawing.Size(897, 307);
+            this.dgvPeople.TabIndex = 124;
+            this.dgvPeople.TabStop = false;
+            // 
+            // pbUserImage
+            // 
+            this.pbUserImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbUserImage.InitialImage = null;
+            this.pbUserImage.Location = new System.Drawing.Point(364, 33);
+            this.pbUserImage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbUserImage.Name = "pbUserImage";
+            this.pbUserImage.Size = new System.Drawing.Size(220, 189);
+            this.pbUserImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbUserImage.TabIndex = 126;
+            this.pbUserImage.TabStop = false;
+            // 
+            // frmListClients
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(920, 652);
+            this.Controls.Add(this.btnAddNewClient);
+            this.Controls.Add(this.cbIsActive);
+            this.Controls.Add(this.cbFilterBy);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pbUserImage);
+            this.Controls.Add(this.txtFilterValue);
+            this.Controls.Add(this.dgvPeople);
+            this.Name = "frmListClients";
+            this.Text = "frmListClients";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPeople)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUserImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddNewClient;
+        private System.Windows.Forms.ComboBox cbIsActive;
+        private System.Windows.Forms.ComboBox cbFilterBy;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pbUserImage;
+        private System.Windows.Forms.TextBox txtFilterValue;
+        private System.Windows.Forms.DataGridView dgvPeople;
+    }
+}
